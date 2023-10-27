c66a2e14af2b3ed95c70055fe7e9701bec78c43f
delviadianitatrianasari <98437064+delviadianitatrianasari@users.noreply.github.com>
2023-10-27 20:32:46 +0700

    Add files via upload

---
+++ PriceCar-Predict.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pickle
+
+# Load the model and data
+model = pickle.load(open("PriceCar-Predict.sav", "rb"))
+
+st.title('Car Price Prediction')
+st.write('Enter the following features to predict the price of the car :')
+
+CarName= st.text_input('Car Company')
+wheelbase= st.number_input('Wheelbase')
+curbweight = st.number_input('Curb Weigh')
+enginesize = st.number_input('Engine Size')
+boreratio = st.number_input('Bore Ratio')
+horsepower = st.number_input('Horse Power')
+carlength = st.number_input('Car Lenght')
+carwidth = st.number_input('Car Widht')
+price = st.number_input('Price')
+ 
+ 
+ 
+ 
+
+predict = ''
+
+if st.button('Price Predict'):
+    predict = model.predict(
+        [[ wheelbase, curbweight, enginesize, boreratio, horsepower, carlength, carwidth, price]]
+    )
+    st.write (' harga mobil dalam USD : ', predict)
+    st.write (' harga mobil dalam IDR (Juta) :', predict*19000)
